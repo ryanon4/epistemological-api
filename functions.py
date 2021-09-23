8099a10397ce1858edccff5fc2c77a46e10bff38
ryanon4 <31044072+ryanon4@users.noreply.github.com>
2021-09-23 13:56:24 +0100

    Fixes & Updates, Arxiv Only

--- functions.py
+++ functions.py
@@ -1,22 +1,27 @@
 import pandas as pd
 from tqdm import tqdm
 import arxiv
+import elsapy
+from elsapy.elsclient import ElsClient
+from elsapy.elsprofile import ElsAuthor, ElsAffil
+from elsapy.elsdoc import FullDoc, AbsDoc
+from elsapy.elssearch import ElsSearch
+import json
 
-def arxiv_search(search_terms, cutoff_date="2010-01-01"):
+def arxiv_search(search_terms, max_results=10000):
     results = []
     for term in tqdm(search_terms):
-        search = arxiv.query(query=term,
-                max_results=10000,
-                start=0,
-                sort_by="relevance",
-                sort_order="descending",
-                prune=True,
-                iterative=False,
-                max_chunk_results=1000)
+        search = arxiv.Search(query=term,
+                max_results=max_results,
+                )
 
         results.append(search)
-    results = [item for sublist in results for item in sublist]
-    results = pd.DataFrame(results)
+    results = [item for sublist in results for item in sublist.results()]
+
+    _results = []
+    for res in results:
+        _results.append({"id": res.entry_id, "title": res.title, "abstract": res.summary, "pubdate": res.published, "pdf_url": res.pdf_url})
+    results = pd.DataFrame(_results)
 
     # Drop all rows not in timeframe
     #results = results[results["published"]# > cutoff_date]
@@ -27,3 +32,23 @@ def arxiv_search(search_terms, cutoff_date="2010-01-01"):
     print("Number of ArXiv Results after removing duplicates: " + str(len(results)))
 
     return results
+
+
+## TODO: UNUSED CURRENTLY ##
+def elsevier_search(search_terms, api_key, cutoff_date="2010-01-01"):
+    client = ElsClient(api_key)
+
+    results = []
+    for term in search_terms:
+        search = ElsSearch(term, "sciencedirect")
+        search.execute(client, get_all=True)
+        results.append(search)
+
+
+    return results
+
+def wiley_search(search_terms, api_key, cutoff_date="2010-01-01"):
+
+    results = []
+    return results
+
--- main.py
+++ main.py
@@ -1,4 +1,8 @@
-from functions import arxiv_search
+from functions import arxiv_search, elsevier_search
+
+
+### API KEYS ###
+els_key = ""
 
 ### Enter your search terms here, list can be as long as you like ###
 search_terms = ['computer supported learning',
@@ -21,8 +25,12 @@ search_terms = ['computer supported learning',
 'educational content management']
 
 # All results are saved to abstracts.csv #
-data = arxiv_search(search_terms)
-data.to_csv("abstracts.csv")
+data = arxiv_search(search_terms, max_results=10)
+data.to_csv("arxiv_output.tsv", sep="\t")
+#elsevier_data = elsevier_search(search_terms, api_key=els_key)
+#elsevier_data.to_csv("elsevier_results.csv")
+
+
 
 
 
